a23eb9d2fbf05d3ecf48de96d71ae7a48e8b1c3c
Andreas Olesen <dresing@live.dk>
2020-03-21 18:38:27 +0100

    Timezone and frequent schedules for test.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->job(new FetchNewData())->everyThirtyMinutes();
+         $schedule->job(new FetchNewData())->everyMinute();
     }
 
     /**
